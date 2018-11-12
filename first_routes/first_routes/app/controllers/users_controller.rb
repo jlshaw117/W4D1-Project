34985952e330b7e945e60586959bb97d6f984958
jlshaw117 <jlshaw117@gmail.com>
2018-11-12 15:36:06 -0800

    Create first route attempts

---
+++ first_routes/first_routes/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    user = User.new(user_params)
+  if user.save
+    render json: user
+  else
+    render json: user.errors.full_messages, status: 418
+  end
+end
+
+  def show
+    user = User.find_by(id: params[:id])  #(params[:id])
+    render json: user.name
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
